66c9875d4133e25a5bea1461b0211f9f68337ed6
Muhammad Usman <muhamusman47@outlook.com>
2025-08-04 08:38:20 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,64 @@
+
+//Question:1
+  
+var x =  "5";
+ var y = 2;
+ var result = x + y;
+ console.log(result); 
+ //Ans: "52"
+ //   // Concatenation because 'a' is a string
+ //  is 'a' converted to number
+
+ //Question:2
+  
+  var x = 10;
+   var y = 5;
+   var z = ++a + b-- + --b;
+   console.log(z); 
+   //Ans: 19;
+
+   //Question:3
+   
+   var Num = ("20") * 3;
+   console.log(Num);
+   // Ans: 60
+   // '20' is converted to a number then multiplied
+
+   //Question:4
+   
+   var a = 4 % 3;
+   console.log(a);
+   // Ans:1 
+   // 4 divided 3 by gives a remainder of 1
+
+
+   //Question:5
+   
+   var studentName = "Hassan";
+   var score = 88;
+  alert(studentName + " got " + score + " marks in JavaScript");
+  //Ans:"Hassan" goot 88 marks in javascript
+
+
+
+  //Question:6
+  
+  var x = 5;
+  var y = x++ + ++x - --x;
+   console.log(y); 
+   //Ans:6 
+   // x++ is post-increment, so it uses the value of x before in the expression.
+
+//Question:8
+
+var priceperPerPerfume = 2500;
+var totalPrice = pricePerPerfume * 4;
+alert("Total Price of 4 Perfume is " + totalPrice + "PKR");
+
+
+//Question:9
+
+ var a = 5;
+ var result = a * 2 + 10 - 3;
+ console.log(result); 
+// Ans :17
